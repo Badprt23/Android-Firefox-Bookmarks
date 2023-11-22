49c9c8e2a459c8e7adef5740f8ff1acc1fb8950a
Badprt23 <96928236+Badprt23@users.noreply.github.com>
2023-11-22 16:43:40 +0100

    Update bookmarks.sql

--- bookmarks.sql
+++ bookmarks.sql
@@ -1,6 +1,6 @@
-/* bookmarks are stored in “/data/data/org.mozilla.firefox/files/mozilla/…….default/browser.db” if you can't find browser.db then places.sqlite is the Treasury u are lookin for */
+/* bookmarks are stored in “/data/data/org.mozilla.firefox/files/mozilla/…….default/browser.db” if you can't find browser.db then places.sqlite is the file containing the bookmarks */
 
 SELECT title, url, created /* or write id instead of created if you have places.sqlite file */
 FROM bookmarks /* write moz_places if you have places.sqlite file */
 WHERE COALESCE(url, '') <> '' /* For places.sqlite file you can ignore or remove this query */
-ORDER BY created ASC
+ORDER BY created ASC  /* replace with id for places.sql */
